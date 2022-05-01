183ec559bdfa59af98bbf09d48adccda77e05cfb
Ronaldeived Silva <ronaldeived.silva@cwi.com.br>
2022-05-01 00:51:16 -0300

    Add project files.

---
+++ ContactAPI.Models/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using ContactAPI.Models.Enums;
+
+namespace ContactAPI.Models
+{
+    public class Contact
+    {
+        public int? Id { get; set; }
+        public ETypeContact? Type { get; set; }
+        public string? Value { get; set; }
+    }
+}
---
+++ ContactAPI/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ContactAPI.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ContactContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDBConnections")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseDeveloperExceptionPage();
+
+app.Run();
